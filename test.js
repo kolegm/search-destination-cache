14e665a7cc5b4eae0dfdf505e71ca924969abfd4
Oleh Kravchuk <kolegm.real@gmail.com>
2014-07-11 00:15:31 +0300

    Improved redis client

--- test.js
+++ test.js
@@ -1,4 +1,17 @@
-var ClientDB = require('./lib/redis');
+var util = require('util');
 
-var clientDB = new ClientDB();
-clientDB.quit();
+var Handler = require('./lib/redis');
+
+var handler = new Handler();
+
+handler.hmsetex('key', {'1':'1','2':'2'}, 60, function (error, data) {
+  if (error) console.log('hmset e: ' + error);
+  else console.log('hmset data: ' + data);
+});
+
+handler.hgetall('key', function (error, data) {
+  if (error) console.log('hgetall e: ' + error);
+  else console.log('hgetall data: ' + util.inspect(data));
+});
+
+handler.quit();
