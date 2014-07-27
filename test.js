17a59bc03c37313077eaeacc5037c47b5e959d0e
Oleh Kravchuk <kolegm.real@gmail.com>
2014-07-27 12:32:00 +0300

    Improved search process

--- test.js
+++ test.js
@@ -1,10 +1,22 @@
 var searcher = require('./');
 
+console.log(new Date());
 var callback = function (error, data) {
   if (error) console.log(error);
-  else console.log(data);
+  else {
+    console.log(new Date());
+    //console.log(data);
+  }
 };
 
-searcher.searchByAddress('kyiv, petra pancha', callback);
-searcher.searchByCoordinates('1.234', '2.345', callback);
+searcher.searchByAddress('kyiv, Khreshchatyk', callback);
+//searcher.searchByCoordinates('21.234', '13.345', callback);
+//searcher.searchByCoordinates('22.234', '13.345', callback);
 
+setTimeout(function () {
+  //searcher.searchByAddress('kyiv, Khreshchatyk', callback);
+}, 10);
+
+setTimeout(function () {
+  console.log('memory usage: ' + process.memoryUsage().heapUsed);
+}, 5000);
